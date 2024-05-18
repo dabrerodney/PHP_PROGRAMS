849074e57b1ce31abce42c7b359cbee812c18447
Rodney Dabre <128955508+dabrerodney@users.noreply.github.com>
2024-05-18 15:07:05 +0530

    first commit

---
+++ LoopsInPhp.php
@@ -0,0 +1,37 @@
+<?php
+//PHP Program to demonstrate the use of LOOPS
+
+//1. While Loop
+$i = 0;
+while ($i < 10)
+{
+    echo "The numbers are ".$i."<br>";
+    $i++;
+}
+echo "<br>";
+
+
+//2. Do while Loop
+$j = 0;
+do{
+    echo "The number is ".$j."<br>";
+    $j++;
+}while($j < 10);
+echo "<br>";
+
+
+//3. For Loop
+for($a = 0; $a<10; $a++)
+{
+    echo "The number is ".$a."<br>";
+}
+echo "<br>";
+
+
+//4. For each Loop
+$colors = array("red", "green", "black", "brown", "grey");
+foreach ($colors as $a)
+{
+    echo "The color is ".$a."<br>";
+}
+?>
---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+$colors = array("red", "yellow", "green");
+echo $colors[2], "<br>";
+echo $colors[1], "<br>";
+
+
+$marks = array("Physics" => 90, "Maths" => 99, "Chemistry" => 95);
+echo $marks["Physics"], "<br>";
+echo $marks["Maths"];
+?>
